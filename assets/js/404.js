ed2b3cfcaac7c8a01e817ebd54468fe51506862c
iltekin <sezer@iltekin.com>
2022-02-11 21:12:48 +0300

    added some javascript code to 404 page

--- assets/js/404.js
+++ assets/js/404.js
@@ -1,12 +1,14 @@
-const targetDiv = document.getElementById("404");
-const text = document.createTextNode("Try this URL: ");
-targetDiv.appendChild(text);
+(function () {
+    const targetDiv = document.getElementById("404");
+    const text = document.createTextNode("Try this URL: ");
+    targetDiv.appendChild(text);
 
-const url = "https://code.iltekin.com" + window.location.pathname;
-const title = document.createTextNode(url);
+    const url = "https://code.iltekin.com" + window.location.pathname;
+    const title = document.createTextNode(url);
 
-const a = document.createElement("a");
-a.href = url;
-a.appendChild(title);
+    const a = document.createElement("a");
+    a.href = url;
+    a.appendChild(title);
 
-targetDiv.appendChild(a);
+    targetDiv.appendChild(a);
+});
